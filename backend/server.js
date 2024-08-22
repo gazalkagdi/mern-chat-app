66839f1af4552a7587fb6378c33e50d95f11cf90
gazalkagdi <gazalkagdi111@gmail.com>
2024-08-22 22:25:30 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-
 import express from "express";
 import cookieParser from "cookie-parser";
+import connectToMongoDB from "./db/connection.js";
+import dotenv from "dotenv";
 
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+dotenv.config();
+
 app.use(express.json()); 
 app.use(cookieParser());
 
 
 app.listen(PORT, () => {
-	
+	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
