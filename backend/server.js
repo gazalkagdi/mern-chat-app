bf18852aee9d5308fbd5d7569c29868d53143354
gazalkagdi <gazalkagdi111@gmail.com>
2024-08-22 22:41:59 +0530

    message api added

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,19 @@ import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connection.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
-
+import messageRoutes from "./routes/message.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
