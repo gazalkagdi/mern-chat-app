ceb12ebbc3b5e3cf5e5ebf6c34bc94bc9b03fc38
gazalkagdi <gazalkagdi111@gmail.com>
2024-08-23 22:02:51 +0530

    socketfix

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -23,6 +23,11 @@ io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
+  if (!userId) {
+    console.error("UserId is required");
+    socket.disconnect();
+    return;
+  }
   if (userId != "undefined") userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all the connected clients
